c3be963644f0897cb735d1d8450256ce52ad50f2
Jessica Odette Guizado Hernández <A01643724@tec.mx>
2024-09-06 00:49:43 -0600

    Coordenadas y limpieza de carpetas

--- Logicadeagentes/app.py
+++ Logicadeagentes/app.py
@@ -3,9 +3,7 @@ import os
 from constants import positions
 from Evidencia2_Sinservidor import start
 import shutil
-import subprocess
 
-global counter
 app = Flask(__name__)
 
 # Folder to save uploaded images
@@ -37,10 +35,9 @@ def upload_image():
 # Route to receive the drone image from Unity (drone views)
 @app.route('/upload_drone_image', methods=['POST'])
 def upload_drone_image():
-    
     if 'image' not in request.files:
         return jsonify({'status': 'error', 'message': 'No image file found'}), 400
-
+    
     image_file = request.files['image']
 
     if image_file.filename == '':
@@ -48,39 +45,11 @@ def upload_drone_image():
     
     # Save the image with the filename provided by Unity
     save_path = os.path.join(UPLOAD_FOLDER_droneViews, image_file.filename)
+    image_file.save(save_path)
     
-    try:
-        # Save the image
-        image_file.save(save_path)
-        print(f"Drone image saved at: {save_path}")  # Log the save action
-        counter+=1
-    except Exception as e:
-        print(f"Error saving the image: {e}")
-        return jsonify({'status': 'error', 'message': f"Failed to save the image: {str(e)}"}), 500
-
-    # Trigger the OpenAI batch processor after the image is uploaded
-    try:
-        # Run the external OpenAI batch script
-        process = subprocess.Popen(['python', '../vision_model/demo.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()  # Capture stdout and stderr
-
-        if process.returncode == 0:
-            print("OpenAI batch processor finished successfully")
-            print(f"Output: {stdout.decode('utf-8')}")
-        else:
-            print(f"OpenAI batch processor failed with return code {process.returncode}")
-            print(f"Error: {stderr.decode('utf-8')}")
-            return jsonify({'status': 'error', 'message': 'Failed to complete OpenAI batch processor'}), 500
-
-    except Exception as e:
-        print(f"Error starting OpenAI batch processor: {e}")
-        return jsonify({'status': 'error', 'message': f"Error starting OpenAI batch processor: {str(e)}"}), 500
+    print(f"Drone image saved at: {save_path}")  # Log the save action
     
-    # Return success response
-    return jsonify({'status': 'success', 'message': f'Drone image saved at {save_path}'}), 200
-
-
-
+    return jsonify({'status': 'success', 'message': f'Drone image saved at {save_path}'})
 
 # Route to clear the folders for starting views and drone images
 @app.route('/clear_folders', methods=['POST'])
@@ -140,7 +109,5 @@ def trigger_capture():
     return jsonify({'status': 'failure', 'message': 'Invalid request'}), 400
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(host='127.0.0.1', port=5000)
-
-
